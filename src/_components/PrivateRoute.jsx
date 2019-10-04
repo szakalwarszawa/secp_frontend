92db1cbdc1ac0732ba318a546ecc09103d97dd5c
Tomek <t.durst@triadis.pl>
2019-10-04 11:44:30 +0200

    fix route for login page

--- src/_components/PrivateRoute.jsx
+++ src/_components/PrivateRoute.jsx
@@ -2,17 +2,28 @@ import React from 'react';
 import { Route, Redirect } from 'react-router-dom';
 
 export const PrivateRoute = ({ component: Component, accessRight, ...rest }) => {
-  if (accessRight) {
-    return (
-      <Route
-        {...rest}
-        render={props => (
-          sessionStorage.getItem('token')
-            ? <Component {...props} />
-            : <Redirect to={{ pathname: '/login', state: { from: props.location } }} />
-        )}
-      />
-    );
-  }
-  return '';
+
+  const getRouteToRender = (props) => {
+    if (!sessionStorage.getItem('token')) {
+      const { location } = props;
+      return (
+        <Redirect to={{ pathname: '/login', state: { from: location } }} />
+      );
+    }
+
+    if (accessRight) {
+      return (
+        <Component {...props} />
+      );
+    }
+
+    return '';
+  };
+
+  return (
+    <Route
+      {...rest}
+      render={props => getRouteToRender(props)}
+    />
+  );
 };
