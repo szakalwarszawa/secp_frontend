222d80f880eb1b0b34c56cea4dc16ae5214b7654
hubertuspp <hubertuspp@gmail.com>
2019-10-30 15:59:48 +0100

    dictionary for messages

--- src/_services/user.service.js
+++ src/_services/user.service.js
@@ -1,6 +1,6 @@
 import { apiService } from './api.service';
 import { userConstants } from '../_constants';
-import {translate} from "../_helpers/translations";
+import { messageTranslate } from '../_helpers/messageTranslations';
 
 function logout() {
   // remove user from local storage to log user out
@@ -12,16 +12,14 @@ function handleResponse(response) {
   return response.text().then((text) => {
     const data = text && JSON.parse(text);
     if (!response.ok) {
+      let error = (data && data.message) || response.statusText;
+
       if (response.status === 401) {
         // auto logout if 401 response returned from api
         logout();
-        // eslint-disable-next-line no-restricted-globals
-        // location.reload(true);
-        const error = (data && translate(data.message)) || response.statusText;
-        return Promise.reject(error);
+        error = (data && messageTranslate(data.message)) || response.statusText;
       }
 
-      const error = (data && data.message) || response.statusText;
       return Promise.reject(error);
     }
 
@@ -85,7 +83,7 @@ function isAdmin() {
     return false;
   }
   const userRoles = getUserData().roles;
-  const ownedAdminRoles = userRoles.filter(value => userConstants.ADMIN_ROLES.includes(value));
+  const ownedAdminRoles = userRoles.filter((value) => userConstants.ADMIN_ROLES.includes(value));
 
   return (ownedAdminRoles.length > 0);
 }
@@ -95,7 +93,7 @@ function isHR() {
     return false;
   }
   const userRoles = getUserData().roles;
-  const ownedAdminRoles = userRoles.filter(value => userConstants.HR_ROLES.includes(value));
+  const ownedAdminRoles = userRoles.filter((value) => userConstants.HR_ROLES.includes(value));
 
   return (ownedAdminRoles.length > 0);
 }
@@ -105,7 +103,7 @@ function isManager() {
     return false;
   }
   const userRoles = getUserData().roles;
-  const ownedAdminRoles = userRoles.filter(value => userConstants.MANAGER_ROLES.includes(value));
+  const ownedAdminRoles = userRoles.filter((value) => userConstants.MANAGER_ROLES.includes(value));
 
   return (ownedAdminRoles.length > 0);
 }
